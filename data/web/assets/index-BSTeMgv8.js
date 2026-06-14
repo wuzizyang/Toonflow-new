1fc159b2538f9ed22676a2adfb0851c11f65be0b
吴紫阳1996 <>
2026-06-14 20:05:13 +0800

    feature：optimize

---
+++ data/web/assets/index-BSTeMgv8.js
@@ -0,0 +1,2 @@
+import{i as n}from"./axios-CGc4JAQE.js";import{B as r}from"./index-D6NHslub.js";import{d as c,c as i,b as m,w as p,e as l,a as _,aJ as f,f as u,y as d,o as x}from"./index-B0e3mtY0.js";const B={class:"test"},C=c({__name:"index",setup(g){const t=d("");function s(){n.get("/test/test").then(e=>{t.value=e})}return(e,o)=>{const a=r;return x(),i("div",B,[m(a,{theme:"primary",onClick:s},{default:p(()=>[...o[0]||(o[0]=[l("测试按钮",-1)])]),_:1}),_("pre",null,"      "+f(u(t))+`
+    `,1)])}}});export{C as default};
